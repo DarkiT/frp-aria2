1e3283bfbe31f5f2979328852cf9fb57bf8f01c3
猴子 <admin@shabi.in>
2018-01-12 23:29:27 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 program_name="frpc"
-version="1.7"
 str_program_dir="/usr/local/bin"
 download_url="https://code.aliyun.com/clangcn/frp/raw/master"
-program_version="0.14.1"
 # Check OS bit
 check_os_bit(){
     echo "check_os_bit"
@@ -19,8 +17,8 @@ check_os_bit(){
 }
 fun_getVer(){
     echo -e "Loading network version for ${program_name}, please wait..."
-    program_latest_filename="frp_${program_version}_linux_${ARCHS}.tar.gz"
-    program_latest_file_url="${download_url}/v${program_version}/${program_latest_filename}"
+    program_latest_filename="frp_${FRP_VER}_linux_${ARCHS}.tar.gz"
+    program_latest_file_url="${download_url}/v${FRP_VER}/${program_latest_filename}"
     if [ -z "${program_latest_filename}" ]; then
         echo -e "${COLOR_RED}Load network version failed!!!${COLOR_END}"
     else
@@ -30,14 +28,14 @@ fun_getVer(){
 fun_download_file(){
     # download
     cd ${str_program_dir}
-    rm -fr ${program_latest_filename} frp_${program_version}_linux_${ARCHS}
+    rm -fr ${program_latest_filename} frp_${FRP_VER}_linux_${ARCHS}
     if ! wget --no-check-certificate -q ${program_latest_file_url} -O ${program_latest_filename}; then
     	echo -e " ${COLOR_RED}failed${COLOR_END}"
 	exit 1
     fi
     tar xzf ${program_latest_filename}
-    mv frp_${program_version}_linux_${ARCHS}/${program_name} ${str_program_dir}/${program_name}
-    rm -fr ${program_latest_filename} frp_${program_version}_linux_${ARCHS}
+    mv frp_${FRP_VER}_linux_${ARCHS}/${program_name} ${str_program_dir}/${program_name}
+    rm -fr ${program_latest_filename} frp_${FRP_VER}_linux_${ARCHS}
     if [ -s ${str_program_dir}/${program_name} ]; then
         [ ! -x ${str_program_dir}/${program_name} ] && chmod 755 ${str_program_dir}/${program_name}
     else
